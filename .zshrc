4e2adf7fddd914b6059276cacf3c3a7e429b3e49
Austin Meadows <austin-meadows@users.noreply.github.com>
2021-12-09 14:30:19 -0500

    updates / no vscode needed

--- .zshrc
+++ .zshrc
@@ -1,11 +1,10 @@
 printf '\33c\e[3J'
 
+HISTSIZE=0
+SAVEHIST=0
+
 export EDITOR=code
 export HOMEBREW_NO_AUTO_UPDATE=1
 export PATH=/opt/homebrew/bin:./node_modules/.bin:$PATH
 
-alias mongo="mongod --config /opt/homebrew/etc/mongod.conf"
-
 PROMPT="\$ "
-
-unsetopt PROMPT_SP
--- install.sh
+++ install.sh
@@ -21,99 +21,37 @@ xcode-select --install
 ############################################## File Operations
 echo "\n${info}Creating folders/files and copying over settings${nocolor}\n"
 # File/Folder creation
-mkdir -p ~/Git
-mkdir -p ~/Library/Application\ Support/Code/User/
+mkdir -p ~/Developer
 
 # Settings downloads
 curl -o ~/.zshrc https://raw.githubusercontent.com/austin-meadows/mac-install-script/main/.zshrc
-cd ~/Library/Application\ Support/Code/User && { curl -O https://raw.githubusercontent.com/austin-meadows/mac-install-script/main/settings.json ; cd -; }
-cd ~/Library/Application\ Support/Code/User && { curl -O https://raw.githubusercontent.com/austin-meadows/mac-install-script/main/keybindings.json ; cd -; }
 
 ############################################## Brew
 echo "\n${info}Installing brew and applications${nocolor}\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew install adguard
 brew install cleanmymac
+brew install git
 brew install homebrew/cask-fonts/font-fira-code
-brew install node
-brew install slack
 brew install visual-studio-code
+brew install volta
 
 ############################################## System
 echo "\n${info}Changing system settings${nocolor}\n"
-## Some items from https://github.com/gricard/new-mac/blob/master/setup.sh
 
-# Don't write DS_STORE on Network or USBs
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+# Change screenshot dir
+defaults write com.apple.screencapture location $HOME/Pictures
+
+# (Hopefully) disable .DS_STORE on USB
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-# Expanded file dialog by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-
-# Save to disk (not to iCloud) by default
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-# Show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-# Enable snap-to-grid for icons on the desktop and in other icon views
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-
-# Use plain text mode for new TextEdit documents
-defaults write com.apple.TextEdit RichText -int 0
-
-# Open and save files as UTF-8 in TextEdit
-defaults write com.apple.TextEdit PlainTextEncoding -int 4
-defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
-
-# Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
-
-# Enable the Develop menu and the Web Inspector in Safari
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true && \
-defaults write com.apple.Safari IncludeDevelopMenu -bool true && \
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true && \
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true && \
-defaults write -g WebKitDeveloperExtras -bool true
-
-# Disable Auto-arrange spaces
-defaults write com.apple.dock mru-spaces -bool false
-
-# Set default search scope to current folder
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-
-# Default new finder window location to ~
-defaults write com.apple.finder NewWindowTarget -string "PfLo" && \
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
-
-# Change screenshot location to ~/Pictures
-defaults write com.apple.screencapture location ~/Pictures
-
-# Kill to refresh
-killall Dock
-killall Finder
-killall SystemUIServer
+# .DS_STORE for network drives?
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 ############################################## Special
 echo "\n${info}Doing a few extra things${nocolor}\n"
-
-echo "\n${info}${bold}Please take a moment to open / allow VSCode to run.${normal}\t${blue}↩ ${nocolor}\n"
-read
-
-# VSCode extensions
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension esbenp.prettier-vscode
-code --install-extension github.github-vscode-theme
-code --install-extension gruntfuggly.activitusbar
-code --install-extension iocave.customize-ui
-code --install-extension octref.vetur
-code --install-extension richie5um2.vscode-sort-json
-code --install-extension vscode-icons-team.vscode-icons
 
 # Git
 git config --global user.name "Austin Meadows"
@@ -121,4 +59,9 @@ git config --global user.email "austin-meadows@users.noreply.github.com"
 
 ############################################## Cleanup
 brew cleanup --prune=0
+# Kill to refresh
+killall Dock
+killall Finder
+killall SystemUIServer
+
 echo "\n${info}Done!${nocolor}\n"
